e2105c15186abc6448bdeebe758c0a3ed8c45f7b
zMiguel151z <pereira.miguel@escolar.ifrn.edu.br>
2025-02-04 20:21:09 -0300

    Update menu.js

--- áreas/Javascript/menu.js
+++ áreas/Javascript/menu.js
@@ -8,7 +8,35 @@ function redirectToChat(farmerName) {
     // Redireciona para a página de chat com o nome do agricultor como parâmetro
     window.location.href = `chat/Index.html?farmer=${encodeURIComponent(farmerName)}`;
 }
+function redirectToperfil(farmerName) {
+    // Redireciona para a página do perfil com o nome do agricultor como parâmetro
+    window.location.href = `Perfil.html?farmer=${encodeURIComponent(farmerName)}`;
+}
 
+function exibir1() {
+    document.getElementById("trabalhos").style.display = "block";
+    document.getElementById("contatos").style.display = "none";
+    document.getElementById("Perfil").style.display = "none";
+    document.getElementById("contatos2").style.display = "none";
+    document.getElementById("Empresas2").style.display = "none";
+        }
 function exibir2() {
     document.getElementById("contatos").style.display = "block";
+    document.getElementById("trabalhos").style.display = "none";
+    document.getElementById("Perfil").style.display = "none";
+    document.getElementById("contatos2").style.display = "none";
+    document.getElementById("Empresas2").style.display = "none";
+        }
+function exibir3() {
+    document.getElementById("Perfil").style.display = "block";
+    document.getElementById("trabalhos").style.display = "none";
+    document.getElementById("contatos").style.display = "none";
+    document.getElementById("contatos2").style.display = "none";
+    document.getElementById("Empresas2").style.display = "none";
+        }
+function exibir4() {
+    document.getElementById("contatos2").style.display = "block";
+        }
+function exibir5() {
+    document.getElementById("Empresas2").style.display = "block";
         }
